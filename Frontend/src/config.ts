60e5d88258a9836660c9555f07995bcc612a9850
Utsav Vasoya <uttasavvasoya@gmail.com>
2025-02-11 05:37:11 +0530

    BAckend Live Checking

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'http://localhost:5000/api'
-export const BASE_URL = 'https://latest-live-code.onrender.com/'
+export const BASE_URL = 'http://localhost:5000/api'
+// export const BASE_URL = 'https://backend-eq3l.onrender.com'
